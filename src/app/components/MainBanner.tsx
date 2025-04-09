59088c3a79e760cd4bf3e7cf849052f9605a9580
KB <111480118+kibrepos@users.noreply.github.com>
2025-04-09 14:02:36 +0800

    Added Main Banner

---
+++ src/app/components/MainBanner.tsx
@@ -0,0 +1,8 @@
+export default function MainBanner() {
+  return (
+    <section className="flex flex-col justify-center items-center text-center h-[80vh] p-20">
+      <h1 className="text-4xl mb-4">Welcome to Sia Succulents</h1>
+      <p className="text-2xl">Your one-stop shop for beautiful succulents!</p>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import FeaturedPlants from "./components/FeaturedPlants";
 import Header from "./components/Header";
+import MainBanner from "./components/MainBanner";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <main>
-      <FeaturedPlants />
+        <MainBanner />
+        <FeaturedPlants />
       </main>
     </div>
   );
